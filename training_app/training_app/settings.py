fae76a656cce75c353c4780b9af05591ab2a3602
Norbert Cyran <cyran.norbert97@gmail.com>
2019-05-11 16:54:20 +0200

    Create accounts app

--- training_app/training_app/settings.py
+++ training_app/training_app/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'knox'
+    'knox',
     'training_app.trainings',
-    'training_app.frontend'
+    'training_app.frontend',
+    'training_app.accounts'
 ]
 
 MIDDLEWARE = [
