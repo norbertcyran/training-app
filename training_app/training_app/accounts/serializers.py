1807fdf90b24973ec7ee491bb913474d41c94f84
Norbert Cyran <cyran.norbert97@gmail.com>
2019-05-11 16:54:59 +0200

    Create accounts serializers

---
+++ training_app/training_app/accounts/serializers.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from rest_framework.serializers import ModelSerializer
+
+
+class UserSerializer(ModelSerializer):
+    """Serializer for User model."""
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+
+class RegisterSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data['username'],
+            validated_data['email'],
+            validated_data['password']
+        )
+
+        return user
