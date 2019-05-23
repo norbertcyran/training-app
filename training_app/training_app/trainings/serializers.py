cbfb518d050f6d341d51cba3cb3d3119607c7e14
cyranekpotasu <cyran.norbert97@gmail.com>
2019-05-24 00:09:07 +0200

    Add serializers for Set and WorkoutExercise

--- training_app/training_app/trainings/serializers.py
+++ training_app/training_app/trainings/serializers.py
@@ -1,7 +1,7 @@
 """Serializers for models."""
 from rest_framework import serializers
 
-from .models import Exercise, MuscleGroup
+from .models import Exercise, MuscleGroup, Set, WorkoutExercise
 
 
 class MuscleGroupSerializer(serializers.ModelSerializer):
@@ -15,3 +15,29 @@ class ExerciseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exercise
         fields = '__all__'
+
+
+class SetSerializer(serializers.ModelSerializer):
+    """Serializer for Set model."""
+    class Meta:
+        model = Set
+        fields = ('reps', 'weight', 'order')
+
+
+class WorkoutExerciseSerializer(serializers.ModelSerializer):
+    """Serializer for WorkoutExercise model."""
+    sets = SetSerializer(many=True)
+
+    class Meta:
+        model = WorkoutExercise
+        fields = ('workout', 'exercise', 'sets')
+
+    def create(self, validated_data):
+        sets = validated_data.pop('sets')
+
+        workout_exercise = WorkoutExercise.objects.create(**validated_data)
+
+        for set_obj in sets:
+            Set.objects.create(workout_exercise=workout_exercise, **set_obj)
+
+        return workout_exercise
