660583792e540c9c4e41da6d1c8fc7b26060c016
cyranekpotasu <cyran.norbert97@gmail.com>
2019-05-24 17:45:21 +0200

    Create nested router for WorkoutExercise

--- training_app/training_app/trainings/urls.py
+++ training_app/training_app/trainings/urls.py
@@ -1,4 +1,5 @@
 from rest_framework import routers
+from rest_framework_nested.routers import NestedSimpleRouter
 
 from . import views
 
@@ -7,4 +8,12 @@ router.register(r'muscle_groups', views.MuscleGroupViewSet)
 router.register(r'exercises', views.ExerciseViewSet)
 router.register(r'workouts', views.WorkoutViewSet, basename='workout')
 
-urlpatterns = router.urls
+exercises_router = NestedSimpleRouter(router, r'workouts', lookup='workout')
+exercises_router.register(
+    r'exercises',
+    viewset=views.WorkoutExerciseViewSet,
+    basename='workout-exercise'
+)
+
+
+urlpatterns = router.urls + exercises_router.urls
