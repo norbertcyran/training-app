248fbda960e783355d833f9d635ac10365038abd
Norbert Cyran <cyran.norbert97@gmail.com>
2019-06-12 00:26:51 +0200

    Add documentation url

--- training_app/training_app/urls.py
+++ training_app/training_app/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 from . import settings
 from .trainings.urls import urlpatterns as trainings_urls
@@ -26,6 +27,7 @@ urlpatterns = [
     path('api/', include('training_app.accounts.urls')),
     path('api/auth/', include('training_app.accounts.auth_urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('docs/', include_docs_urls('Workout manager API')),
     path('', include('training_app.frontend.urls'))
 ]
 
