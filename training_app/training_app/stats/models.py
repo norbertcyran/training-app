e63633f9ac80319771174e9694611e003811c4ed
Norbert Cyran <cyran.norbert97@gmail.com>
2019-06-12 00:40:44 +0200

    Add stats app

---
+++ training_app/training_app/stats/models.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from ..trainings.models import Exercise
+
+
+class WeightEntry(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='weight_entries'
+    )
+
+    weight = models.DecimalField(
+        verbose_name=_('Weight'),
+        decimal_places=1,
+        max_digits=4
+    )
+
+    date = models.DateField(auto_now_add=True)
+
+
+class ExerciseScoreEntry(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='records'
+    )
+
+    exercise = models.ForeignKey(
+        Exercise,
+        on_delete=models.CASCADE,
+        related_name='record',
+        help_text=_('Exercise'),
+        verbose_name=_('Exercise')
+    )
+
+    weight = models.DecimalField(
+        verbose_name=_('Weight'),
+        decimal_places=2,
+        max_digits=5,
+        help_text=_('Weight lifted'),
+    )
