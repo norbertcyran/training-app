71a6ddb6fc1446f268234924cedb0646ff48e72e
cyranekpotasu <cyran.norbert97@gmail.com>
2019-06-17 22:57:07 +0200

    Move whitenoise middleware

--- training_app/training_app/settings/production.py
+++ training_app/training_app/settings/production.py
@@ -8,8 +8,6 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'npng-workouts.herokuapp.com']
 
-MIDDLEWARE += 'whitenoise.middleware.WhiteNoiseMiddleware'
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
