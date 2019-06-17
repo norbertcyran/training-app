56b0f003fa84c3236b5807892ede7dc47091d128
cyranekpotasu <cyran.norbert97@gmail.com>
2019-06-17 22:21:11 +0200

    Add static files storage for heroku

--- training_app/training_app/settings/production.py
+++ training_app/training_app/settings/production.py
@@ -7,3 +7,7 @@ DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'npng-workouts.herokuapp.com']
+
+MIDDLEWARE += 'whitenoise.middleware.WhiteNoiseMiddleware'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
